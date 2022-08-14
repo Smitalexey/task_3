217cec82bc9f2b00e5b0dc2063d39e0e024dd98c
Aleksey <smit_alexey@mail.ru>
2022-08-14 21:18:07 +0500

    Проверка палиндрома

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите любое пятизначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        string numberText = Convert.ToString(number);
+        if ((number[0] = numberText[4]) && (number[1] = number[3]))
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+            {
+                Console.WriteLine("Число не является палиндромом");
+            }
+    }
+}
