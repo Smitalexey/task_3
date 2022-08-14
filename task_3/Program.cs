dbb2d498b3eec793edca1a99467e718dd6d89ae7
Aleksey <smit_alexey@mail.ru>
2022-08-14 09:34:08 +0500

    Возведение чисел в куб по порядку

---
+++ task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите любое натуральное число  ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];  int[] b = new int[n];
+        for (int i = 0; i <= n; i++)
+        {
+            a[i] = i+1; b[i] = a[i]*a[i]*a[i];
+            Console.WriteLine("Числа от 1 до n: " + a[i] + ", " + "числа от 1 до n в кубе: " + b[i]);
+        }
+            Console.ReadKey();
+    }
+}
